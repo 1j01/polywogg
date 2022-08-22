d41e4726d03c099364fb7af2272bef5d7484202c
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-22 18:06:08 -0400

    sigh
    w4 png2src command failed:
     /usr/bin/env: ‘node’: No such file or directory

--- src/png2mem.py
+++ src/png2mem.py
@@ -8,18 +8,42 @@
 
 # TODO: look into using libscanmem, rather than the scanmem CLI
 
+import argparse
+import configparser
+import re
 import getpass
+import os
+from subprocess import run, Popen, PIPE, STDOUT
+
+
+parser = argparse.ArgumentParser(description = "Splice data into memory between a start and end sentinel.")
+
+parser.add_argument("--w4", metavar = "path", type = str, help = "path to the WASM-4 binary 'w4'")
+
+args = parser.parse_args()
+
+if args.w4 and args.w4 != "":
+    w4 = args.w4
+else:
+	# This doesn't work with sudo, even with sudo -E
+	w4_paths = run(["which", "w4"], capture_output=True).stdout.decode().splitlines()
+	if len(w4_paths) == 0:
+		print("w4 not found in PATH! Use --w4 to tell this script where w4 is.")
+		print("PATH: ", os.environ["PATH"])
+		exit(1)
+	w4 = w4_paths[0]
+
+
 if getpass.getuser() != "root":
-	# print("This script must be run as root. Use `sudo -E python3 png2mem.py`")
+	# print("This script must be run as root. Use `sudo python3 png2mem.py`")
 	# exit(1)
-	print("This script should be run as root, using `sudo -E python3 png2mem.py`")
+	print("This script should be run as root, using `sudo python3 png2mem.py`")
 	print("Continuing on, in case it works anyways...")
 
-import configparser
+
 config = configparser.ConfigParser()
 config.read('build/png2src-generated/png2mem.ini')
 
-from subprocess import Popen, PIPE, STDOUT, run
 
 target_p_name = 'wasm4-linux'
 matching_pids = run(['pgrep', target_p_name], capture_output=True).stdout.decode().splitlines()
@@ -53,13 +77,10 @@ scanmem_commands=[
 	END_SENTINEL_BYTES_SCANMEM,
 	"list",
 ]
-# p.stdin.writelines([line.encode() for line in scanmem_commands])
-# p.stderr.readlines()
 stdout_data, stderr_data = p.communicate(input="\n".join(scanmem_commands).encode())
 print("stdout:\n    " + "\n    ".join(stdout_data.decode().splitlines()))
 print("stderr:\n    " + "\n    ".join(stderr_data.decode().splitlines()))
 
-import re
 matches = re.findall(r"\[\s*\d+\]\s([0-9a-fA-F]+),.*,\s*((?:[0-9a-fA-F]{2}\s*)+),\s*\[bytearray\]", stdout_data.decode())
 print("matches:", matches)
 
@@ -80,20 +101,15 @@ for match in matches:
 print("starts:", starts)
 print("ends:", ends)
 
-w4_paths = run(["which", "w4"], capture_output=True).stdout.decode().splitlines()
-print("w4 program path(s): ", w4_paths)
-if len(w4_paths) == 0:
-	print("w4 not found in PATH!")
-	import os
-	print("PATH: ", os.environ["PATH"])
-	print("This script must be run with sudo -E flag to preserve the environment variables.")
-	exit(1)
-
 # get new bytes to splice in
-image_data_str = run([w4, "png2src", "--template", "src/png2src-template.txt.mustache", target_file], capture_output=True).stdout.decode()
+ran = run([w4, "png2src", "--template", "src/png2src-template.txt.mustache", target_file], capture_output=True)
+image_data_str = ran.stdout.decode()
+if len(ran.stderr) > 0:
+	print("w4 png2src command failed:\n", ran.stderr.decode())
+	exit(1)
 # "0x00, 0xFF" -> "00 FF"
-image_data_bytes_scanmem = re.findall(r"0x([0-9a-fA-F]{2})")
-print(image_data_bytes_scanmem)
-print(image_data_bytes_scanmem.zip())
+image_data_bytes_scanmem = re.findall(r"0x([0-9a-fA-F]{2})", image_data_str)
+print("image_data_str:", image_data_str)
+print("image_data_bytes_scanmem:", image_data_bytes_scanmem)
 
 
