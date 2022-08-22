b37d99d7b1edf53541b53985ff5c82ecbbc3829c
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-22 18:46:43 -0400

    so damn close...

--- src/png2mem.py
+++ src/png2mem.py
@@ -22,22 +22,24 @@ parser.add_argument("--w4", metavar = "path", type = str, help = "path to the WA
 
 args = parser.parse_args()
 
+recommended_cmd = 'sudo env "PATH=$PATH" python3 src/png2mem.py'
+
 if args.w4 and args.w4 != "":
     w4 = args.w4
 else:
-	# This doesn't work with sudo, even with sudo -E
 	w4_paths = run(["which", "w4"], capture_output=True).stdout.decode().splitlines()
 	if len(w4_paths) == 0:
-		print("w4 not found in PATH! Use --w4 to tell this script where w4 is.")
+		print("w4 not found in PATH!")
+		print("Note that this script must be run with `%s` in order to preserve the PATH." % (recommended_cmd,))
 		print("PATH: ", os.environ["PATH"])
 		exit(1)
 	w4 = w4_paths[0]
 
 
 if getpass.getuser() != "root":
-	# print("This script must be run as root. Use `sudo python3 png2mem.py`")
+	# print("This script must be run as root. Use `%s`" % (recommended_cmd,))
 	# exit(1)
-	print("This script should be run as root, using `sudo python3 png2mem.py`")
+	print("This script should be run as root, using `%s`" % (recommended_cmd,))
 	print("Continuing on, in case it works anyways...")
 
 
@@ -84,13 +86,14 @@ print("stderr:\n    " + "\n    ".join(stderr_data.decode().splitlines()))
 matches = re.findall(r"\[\s*\d+\]\s([0-9a-fA-F]+),.*,\s*((?:[0-9a-fA-F]{2}\s*)+),\s*\[bytearray\]", stdout_data.decode())
 print("matches:", matches)
 
-starts = []
-ends = []
+starts = [] # start of range (i.e. location AFTER start sentinel), not location of start sentinel
+ends = [] # end of range == location of end sentinel
+NUM_START_SENTINEL_BYTES = len(START_SENTINEL_BYTES_SCANMEM.split(" "))
 for match in matches:
 	if match[1].lower().strip() == START_SENTINEL_BYTES_SCANMEM.lower().strip():
-		starts.append(match[0])
+		starts.append(int(match[0], 16) + NUM_START_SENTINEL_BYTES)
 	elif match[1].lower().strip() == END_SENTINEL_BYTES_SCANMEM.lower().strip():
-		ends.append(match[0])
+		ends.append(int(match[0], 16))
 	else:
 		print("Warning: matched memory address that doesn't match the search bytes!")
 		print("Address:                           "+match[0])
@@ -103,13 +106,37 @@ print("ends:", ends)
 
 # get new bytes to splice in
 ran = run([w4, "png2src", "--template", "src/png2src-template.txt.mustache", target_file], capture_output=True)
-image_data_str = ran.stdout.decode()
+image_data_str = ran.stdout.decode().strip()
 if len(ran.stderr) > 0:
 	print("w4 png2src command failed:\n", ran.stderr.decode())
 	exit(1)
 # "0x00, 0xFF" -> "00 FF"
-image_data_bytes_scanmem = re.findall(r"0x([0-9a-fA-F]{2})", image_data_str)
+image_data_bytes = [int(byte_str, 0) for byte_str in image_data_str.split(",")]
+image_data_bytes_scanmem = " ".join(re.findall(r"0x([0-9a-fA-F]{2})", image_data_str))
 print("image_data_str:", image_data_str)
 print("image_data_bytes_scanmem:", image_data_bytes_scanmem)
 
+# match up starts and ends, and sanity-check length of ranges
+valid_starts = []
+for start in starts:
+	valid = False
+	for end in ends:
+		if end - start == len(image_data_bytes):
+			valid = True
+	if valid:
+		valid_starts.append(start)
+	else:
+		print("Warning: invalid range - didn't find an end sentinel %d bytes after the start sentinel %x" % (len(image_data_bytes), start))
+
+# splice the bytes into the program's memory
+scanmem_commands=["reset"]
+for start in valid_starts:
+	scanmem_commands.append("write bytearray %x %s" % (start, image_data_bytes_scanmem))
+
+# need a new process if using communicate()
+p = Popen(['scanmem', '--pid', target_pid], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+
+stdout_data, stderr_data = p.communicate(input="\n".join(scanmem_commands).encode())
+print("stdout:\n    " + "\n    ".join(stdout_data.decode().splitlines()))
+print("stderr:\n    " + "\n    ".join(stderr_data.decode().splitlines()))
 
