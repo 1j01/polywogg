32e5cb61c768632917f6539e3757f79ff75a4481
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-22 01:41:46 -0400

    sentinel per sprite

--- src/png2src.sh
+++ src/png2src.sh
@@ -11,5 +11,20 @@ rm $FOLDER/*
 for f in src/png/*.png
 do
 	# Had to use piping since --output option didn't work for me.
-	w4 png2src --assemblyscript $f --template src/png2src-template.ts.mustache > $FOLDER/$(basename $f .png).ts
+
+	# Alright, I'm doing something crazy here:
+	# I'm using `w4 png2src`'s built-in mustache template support
+	# but then also modifying the output with `sed` here.
+	# So it's like a double templated file.
+	# I'm also doing this in order to allow for scanmem, which is pretty crazy.
+	START_SENTINEL_BYTES=$(echo -n "MEM_START_SENTINEL" "$f" | xxd -i | tr -d '\r\n')
+	END_SENTINEL_BYTES=$(echo -n "MEM_START_SENTINEL" "$f" | xxd -i | tr -d '\r\n')
+	# echo "START_SENTINEL_BYTES: $START_SENTINEL_BYTES"
+	# echo "END_SENTINEL_BYTES: $END_SENTINEL_BYTES"
+	w4 png2src \
+		--assemblyscript $f \
+		--template src/png2src-template.ts.mustache \
+		| sed "s/START_SENTINEL_BYTES/$START_SENTINEL_BYTES/g" \
+		| sed "s/END_SENTINEL_BYTES/$END_SENTINEL_BYTES/g" \
+		> $FOLDER/$(basename $f .png).ts
 done
