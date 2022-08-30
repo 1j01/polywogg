7cdc33656c05ca15ed942281f7b199a46c4aff3a
Isaiah Odhner <isaiahodhner@gmail.com>
2022-08-29 22:06:00 -0400

    Fix sword facing direction in hit check

--- src/main.ts
+++ src/main.ts
@@ -196,7 +196,7 @@ function updatePlayer(player: Player): void {
                 otherPlayer.stance == player.stance &&
                 otherPlayer.facing !== player.facing;
             if (
-                Math.abs(otherPlayer.x - player.x + player.facing as i32 * 5) < 9 &&
+                Math.abs(otherPlayer.x - (player.x + (player.facing as i32 * 5))) < 3 &&
                 Math.abs(otherPlayer.y - player.y) < 9 &&
                 otherPlayer.stunTimer <= 0 && // TODO: separate invincibility timer, or prevent double hits by tracking whether the lunge has hit a player
                 !otherPlayer.dead
